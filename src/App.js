a25e0b097106c1cc6773e7412471ceb23199964b
Eliezer <luccas_passos@hotmail.com>
2022-05-30 23:06:36 -0300

    comit inciarl

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from  'react';
+import './App.css';
+
+function App() {
+  return (
+    <header>
+      <h1>Espaco Delas</h1>
+      <ul>
+        <li>About</li>
+        <li>Galeria</li>
+        <li>Contatc</li>
+      </ul>
+    </header>
+  );
+}
+
+export default App;
